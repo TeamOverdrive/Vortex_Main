8b9ea3bb3a3dbdcc93e436c3b4179aaa2326c48e
TeamOverdrive <overdrive2753@gmail.com>
2017-01-27 17:16:48 -0500

    added new turn radius methods and new autodrive subclass for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_New_B1_B2_S2_C.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_New_B1_B2_S2_C.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 /**
- * Created by Samuel Turner on 1/6/2017.
+ * Created by S Turner on 1/27/2017.
  */
 
 @Autonomous(name = "AutoDrive-Blue: New P1 Beacon2 Shoot2 Center", group = "Vortex")
@@ -15,20 +15,21 @@ public class AutoDrive_New_B1_B2_S2_C extends AutoSuper {
         super.runOpMode();
         calibrateGyro();
         waitForStart();
-        //Step 1: Launch the balls.
-        encoderDrive(DRIVE_SPEED, 41.0, 41.0, 5.0);
-        //launchBalls(2);
-        //Step 2: Get between the beacons.
-        turnGyroAbsR(45);
-        launchBalls(2);
-        //encoderDrive(DRIVE_SPEED, -18.0, -18.0, 5.0);
-        //turn90L();
-        //encoderDrive(DRIVE_SPEED, -36.0, -36.0, 5.0);
-        //turnGyroAbsR(90);
-        //Needs adjustment
-        //encoderDrive(DRIVE_SPEED, -26.0, -26.0, 3.0);
-        //turnGyroAbsR(180);
-        //Step 3: Hit the beacons.
+
+        //Step 1: Drive to side wall to position for beacon pushing.
+        gyroTurn(TURN_SPEED, -45.0);
+        gyroDrive(DRIVE_SPEED, -41.0, 0.0);
+        gyroTurn(TURN_SPEED, 45.0);
+
+        //Step 2:  Run beacon push routine
         pushBeaconBackward(true);
+
+        //Step 3:  Position for and launch the balls
+        gyroTurn(TURN_SPEED, 90.0);
+        gyroDrive(DRIVE_SPEED, 12.0, 0.0);
+        launchBalls(2);
+
+        //Step 4:  Drive to center and stop
+        gyroDrive(DRIVE_SPEED, 24.0, 0.0);
     }
 }
