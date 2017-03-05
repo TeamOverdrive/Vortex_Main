30fdbb9e8d0e4d3bd86992dca8ee834dfc078cc5
TeamOverdrive <overdrive2753@gmail.com>
2017-03-05 15:43:46 -0500

    added new turn radius methods and new autodrive subclass for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_Test_RED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive_Test_RED.java
@@ -22,10 +22,10 @@ public class AutoDrive_Test_RED extends AutoSuper {
         launchBalls(2);
         encoderDrive(DRIVE_SPEED * 0.7, -7.0, -7.0, 3.0);
         turn45L();
-        encoderDrive(DRIVE_SPEED * 0.7, -47.5, -47.5, 5.0);
+        encoderDrive(DRIVE_SPEED * 0.7, -45.0, -45.0, 5.0);
         encoderDrive(DRIVE_SPEED * 0.2, -4.0, -4.0, 3.0);
         turn135R_RED();
-        encoderDrive(DRIVE_SPEED * 0.7, 11.0, 11.0, 5.0);
+        encoderDrive(DRIVE_SPEED * 0.4, 11.0, 11.0, 5.0);
         sleep(500);
         encoderDrive(DRIVE_SPEED * 0.4, -5.5, -5.5, 3.0);
         turn90L_RED();
